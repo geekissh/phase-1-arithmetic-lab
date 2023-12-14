2f478cfb348c3c2d4200cae844feaba2ea06169b
Mark Kelsall <99473523+geekissh@users.noreply.github.com>
2023-12-14 09:02:12 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// Ch 1
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Ch 2
+let random = Math.floor(Math.random() * 10) + 1;
+
+// Ch 3
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+// Ch 4
+let max = Math.max(2, 5, 8, 11, 20);
+
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max,
+};
+
